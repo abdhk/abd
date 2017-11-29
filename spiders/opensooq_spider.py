690b8d50516da44a5bc8eacafead19fcce89d9ee
aeweiwi <abdalrahman.eweiwi@gmail.com>
2017-11-29 01:06:49 +0100

    Make further changes on the spider

--- spiders/opensooq_spider.py
+++ spiders/opensooq_spider.py
@@ -10,12 +10,16 @@ from opensooq.items import OpenSooqCar
 class Opensooq(BaseSpider):
     name = "opensooq"
     allowed_domains = ["opensooq.com"]
-    start_urls = ["https://sa.opensooq.com/ar/%D8%AD%D8%B1%D8%A7%D8%AC-%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9",
-                  "https://eg.opensooq.com/ar/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%88%D9%85%D8%B1%D9%83%D8%A8%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9",
-                  "https://lb.opensooq.com/ar/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%88%D9%85%D8%B1%D9%83%D8%A8%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9"]
-
+    start_urls = ("https://sa.opensooq.com/ar/%D8%AD%D8%B1%D8%A7%D8%AC-%D8%A7%D9%84%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9",)
+    """
+    "https://eg.opensooq.com/ar/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%88%D9%85%D8%B1%D9%83%D8%A8%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9",
+                  "https://lb.opensooq.com/ar/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%88%D9%85%D8%B1%D9%83%D8%A8%D8%A7%D8%AA/%D8%B3%D9%8A%D8%A7%D8%B1%D8%A7%D8%AA-%D9%84%D9%84%D8%A8%D9%8A%D8%B9"
+    """
     
     cars_list_xpath = "//div[@class='galleryCont specialPost']"
+
+    cars_menue_xpath = "//li[@class='rectLi ie relative mb15']/div/div/a/@href"
+
     item_fields = {'city' : ".//div[@class='customP']/ul/li[1]/a/text()",
                    'brand' : ".//div[@class='customP']/ul/li[2]/a/text()",
                    'model' : ".//div[@class='customP']/ul/li[3]/a/text()",
@@ -28,8 +32,15 @@ class Opensooq(BaseSpider):
                    'description' : ".//div[@class='albumDet clear']/div[@class='postDesc']/p/text()"
                    }
 
+    def parse(self,response):
+        selector=HtmlXPathSelector(response)
+        for car in selector.select(self.cars_menue_xpath):
+            car_url = car.xpath('.//div/div/a/@href').extract()
+            url_sub = urljoin(response.url, car_url)
+            yield (url_sub, callback='parse_sub')
 
-    def parse(self, response):
+
+    def parse_sub(self, response):
 
         selector = HtmlXPathSelector(response)
 
